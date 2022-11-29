019f22b5ad2350cb505b63133e328bd96ca587bb
Dimitris Koutsoukos <dkoutsou@inf.ethz.ch>
2022-11-29 17:39:26 +0100

    Cleanup include folder

--- include/cvm-c/Dialects.h
+++ include/cvm-c/Dialects.h
@@ -1,4 +1,4 @@
-//===- Passes.h - CAPI for passes ---------------------------------*- C -*-===//
+//===- Dialects.h - CAPI for dialects -----------------------------*- C -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,21 +6,32 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef ITERATORS_C_PASSES_H
-#define ITERATORS_C_PASSES_H
+#ifndef CVM_C_DIALECTS_H
+#define CVM_C_DIALECTS_H
+
+#include "mlir-c/IR.h"
+#include "mlir-c/Registration.h"
+#include "mlir-c/Support.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 //===----------------------------------------------------------------------===//
-// Iterators passes
+// CVM dialect
 //===----------------------------------------------------------------------===//
 
-#include "iterators/Conversion/Passes.capi.h.inc" // IWYU pragma: export
+MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(Cvm, cvm);
+
+MLIR_CAPI_EXPORTED
+bool mlirTypeIsACvmStreamType(MlirType type);
+
+MLIR_CAPI_EXPORTED
+MlirType mlirCvmStreamTypeGet(MlirContext context, MlirType elementType);
+
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // ITERATORS_C_PASSES_H
+#endif // CVM_C_DIALECTS_H
---
+++ include/cvm/Dialect/Cvm/IR/Cvm.h
@@ -0,0 +1,33 @@
+//===-- CvmDialect.h - Cvm dialect ------------------*- C++ -*-===//
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CVM_DIALECT_CVM_IR_CVM_H
+#define CVM_DIALECT_CVM_IR_CVM_H
+
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+
+#include "cvm/Dialect/Cvm/IR/CvmOpsDialect.h.inc"
+
+namespace mlir {
+namespace cvm {
+#include "cvm/Dialect/Cvm/IR/CvmOpInterfaces.h.inc"
+#include "cvm/Dialect/Cvm/IR/CvmTypeInterfaces.h.inc"
+} // namespace cvm
+} // namespace mlir
+
+#define GET_TYPEDEF_CLASSES
+#include "cvm/Dialect/Cvm/IR/CvmOpsTypes.h.inc"
+
+#define GET_OP_CLASSES
+#include "cvm/Dialect/Cvm/IR/CvmOps.h.inc"
+
+#endif // CVM_DIALECT_CVM_IR_CVM_H
