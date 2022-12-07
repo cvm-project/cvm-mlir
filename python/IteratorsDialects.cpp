ecaf0083507aa1cf05d37d91ec22d46398f55d67
Dimitris Koutsoukos <dkoutsou@inf.ethz.ch>
2022-12-07 08:59:43 +0100

    More cleanup on python folder

--- python/IteratorsDialects.cpp
+++ python/IteratorsDialects.cpp
@@ -1,4 +1,4 @@
-//===- IteratorsExtension.cpp - Extension module --------------------------===//
+//===- CvmExtension.cpp - Extension module --------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -11,7 +11,7 @@
 #include <array>
 #include <initializer_list>
 
-#include "iterators-c/Dialects.h"
+#include "cvm-c/Dialects.h"
 #include "mlir-c/IR.h"
 #include "mlir-c/Registration.h"
 #include "mlir/Bindings/Python/PybindAdaptors.h"
@@ -19,20 +19,20 @@
 namespace py = pybind11;
 using namespace mlir::python::adaptors;
 
-PYBIND11_MODULE(_iteratorsDialects, mainModule) {
+PYBIND11_MODULE(_cvmDialects, mainModule) {
   //===--------------------------------------------------------------------===//
-  // Iterators dialect
+  // Cvm dialect
   //===--------------------------------------------------------------------===//
-  auto iteratorsModule = mainModule.def_submodule("iterators");
+  auto cvmModule = mainModule.def_submodule("cvm");
 
   //
   // Dialect
   //
 
-  iteratorsModule.def(
+  cvmModule.def(
       "register_dialect",
       [](MlirContext context, bool doLoad) {
-        MlirDialectHandle handle = mlirGetDialectHandle__iterators__();
+        MlirDialectHandle handle = mlirGetDialectHandle__cvm__();
         mlirDialectHandleRegisterDialect(handle, context);
         if (doLoad) {
           mlirDialectHandleLoadDialect(handle, context);
@@ -44,57 +44,15 @@ PYBIND11_MODULE(_iteratorsDialects, mainModule) {
   // Types
   //
 
-  mlir_type_subclass(iteratorsModule, "StreamType",
-                     mlirTypeIsAIteratorsStreamType)
+  mlir_type_subclass(cvmModule, "StreamType",
+                     mlirTypeIsACvmStreamType)
       .def_classmethod(
           "get",
           [](const py::object &cls, MlirType elementType, MlirContext context) {
-            return cls(mlirIteratorsStreamTypeGet(context, elementType));
+            return cls(mlirCvmStreamTypeGet(context, elementType));
           },
           py::arg("cls"), py::arg("element_type"),
           py::arg("context") = py::none());
 
-  //===--------------------------------------------------------------------===//
-  // Tabular dialect.
-  //===--------------------------------------------------------------------===//
-  auto tabularModule = mainModule.def_submodule("tabular");
-
-  //
-  // Dialect
-  //
-
-  tabularModule.def(
-      "register_dialect",
-      [](MlirContext context, bool doLoad) {
-        MlirDialectHandle handle = mlirGetDialectHandle__tabular__();
-        mlirDialectHandleRegisterDialect(handle, context);
-        if (doLoad) {
-          mlirDialectHandleLoadDialect(handle, context);
-        }
-      },
-      py::arg("context") = py::none(), py::arg("load") = true);
-
-  //
-  // Types
-  //
-
-  mlir_type_subclass(tabularModule, "TabularViewType", mlirTypeIsATabularView)
-      .def_classmethod(
-          "get",
-          [](const py::object &cls, const py::list &columnTypeList,
-             MlirContext context) {
-            intptr_t num = py::len(columnTypeList);
-            // Mapping py::list to SmallVector.
-            llvm::SmallVector<MlirType, 4> columnTypes;
-            for (auto columnType : columnTypeList) {
-              columnTypes.push_back(columnType.cast<MlirType>());
-            }
-            return cls(
-                mlirTabularViewTypeGet(context, num, columnTypes.data()));
-          },
-          py::arg("cls"), py::arg("column_types"),
-          py::arg("context") = py::none())
-      .def("get_column_type", mlirTabularViewTypeGetColumnType, py::arg("pos"))
-      .def("get_num_column_types", mlirTabularViewTypeGetNumColumnTypes)
-      .def("get_row_type", mlirTabularViewTypeGetRowType);
+  
 }
